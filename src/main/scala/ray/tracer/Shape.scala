2ad6686b32949c40d90dfcb6f8a30a91422751b6
Oleg Prozorov <pro.oleg@gmail.com>
2020-04-05 15:31:45 +0200

    Extract ShapeFactory

--- src/main/scala/ray/tracer/Shape.scala
+++ src/main/scala/ray/tracer/Shape.scala
@@ -51,66 +51,6 @@ case class SimpleShape(math: ShapeMath,
     math.normalAt(point, Option(intersection).map(i => Inter(i.t, i.u, i.v)).orNull)
 }
 
-object ShapeFactory {
-
-  def glassSphere(transform: Matrix4x4 = Matrix4x4.Identity,
-                  material: Material = Material(transparency = 1.0, refractiveIndex = 1.5)): Shape =
-    Sphere(transform, material)
-
-  def Sphere(transform: Matrix4x4 = Matrix4x4.Identity,
-             material: Material = Material(),
-             parent: Shape = null): Shape =
-    SimpleShape(SphereMath(), transform, material, parent)
-
-  def Plane(transform: Matrix4x4 = Matrix4x4.Identity,
-            material: Material = Material(),
-            parent: Shape = null): Shape =
-    SimpleShape(PlaneMath(), transform, material, parent)
-
-  def Cube(transform: Matrix4x4 = Matrix4x4.Identity,
-           material: Material = Material(),
-           parent: Shape = null): Shape =
-    SimpleShape(CubeMath(), transform, material, parent)
-
-  def Cylinder(minimum: Double = Double.NegativeInfinity,
-               maximum: Double = Double.PositiveInfinity,
-               closed: Boolean = false,
-               transform: Matrix4x4 = Matrix4x4.Identity,
-               material: Material = Material(),
-               parent: Shape = null): Shape =
-    SimpleShape(CylinderMath(minimum, maximum, closed), transform, material, parent)
-
-  def Cone(minimum: Double = Double.NegativeInfinity,
-           maximum: Double = Double.PositiveInfinity,
-           closed: Boolean = false,
-           transform: Matrix4x4 = Matrix4x4.Identity,
-           material: Material = Material(),
-           parent: Shape = null): Shape =
-    SimpleShape(ConeMath(minimum, maximum, closed), transform, material, parent)
-
-  def Triangle(p1: Point,
-               p2: Point,
-               p3: Point,
-               transform: Matrix4x4 = Matrix4x4.Identity,
-               material: Material = Material(),
-               parent: Shape = null): Shape =
-    SimpleShape(TriangleMath(p1, p2, p3), transform, material, parent)
-
-  def SmoothTriangle(p1: Point,
-                     p2: Point,
-                     p3: Point,
-                     n1: Vector,
-                     n2: Vector,
-                     n3: Vector,
-                     transform: Matrix4x4 = Matrix4x4.Identity,
-                     material: Material = Material(),
-                     parent: Shape = null): Shape =
-    SimpleShape(SmoothTriangleMath(p1, p2, p3, n1, n2, n3), transform, material, parent)
-
-}
-
-
-
 //todo implement as immutable, with builder
 case class Group(children: ListBuffer[Shape] = ListBuffer(),
                  transform: Matrix4x4 = Matrix4x4.Identity,
